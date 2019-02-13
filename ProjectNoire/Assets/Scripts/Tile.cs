1b79da36454ddd46aab670bfa7aebec66825cf15
Altenkern <bednuy99@gmail.com>
2019-02-13 17:45:12 +0200

    Initial commit

---
+++ ProjectNoire/Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    private int xPos;
+    private int yPos;
+    private int charId;//character
+
+    public Tile(int xPosition, int yPosition) { xPos = xPosition; yPos = yPosition; }
+
+    public Tile(int xPosition, int yPosition, int characterId) { xPos = xPosition; yPos = yPosition; charId = characterId; }
+
+    public int[] getInfo()
+    {
+        int[] arr = { xPos, yPos, charId };
+        return arr;
+    }
+}
